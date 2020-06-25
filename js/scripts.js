06f513e6b00c049d2f5612577e3eb2f113d038e1
junior-rijo <alberino.rijo@outlook.com>
2020-06-24 22:28:10 -0300

    add_aula_11

--- js/scripts.js
+++ js/scripts.js
@@ -106,7 +106,46 @@ $(document).ready(function () {
 
     // setTimeout serve para carregar primeiro as imagens
     setTimeout(function () {
-        $('#data-area').parallax({ imageSrc: 'Imagens/cidadeparallax.png' });
-        $('#apply-area').parallax({ imageSrc: 'img/pattern.png' });
-    }, 200);
-})
+        $('#data-area').parallax({ imageSrc: './Imagens/cidadeparallax.png' });
+        $('#apply-area').parallax({ imageSrc: 'Imagens/pattern.png' });
+    }, 250);
+    // Filtro do Portfólio
+
+    $('.filter-btn').on('click', function () {
+
+        let type = $(this).attr('id');
+        let boxes = $('.project-box');
+
+        $('.main-btn').removeClass('active');
+        $(this).addClass('active');
+
+        if (type == 'dsg-btn') {
+            eachBoxes('dsg', boxes);
+        } else if (type == 'dev-btn') {
+            eachBoxes('dev', boxes);
+        } else if (type == 'seo-btn') {
+            eachBoxes('seo', boxes);
+        } else {
+            eachBoxes('all', boxes);
+        }
+
+    });
+
+    function eachBoxes(type, boxes) {
+
+        if (type == 'all') {
+            $(boxes).fadeIn();
+        } else {
+            $(boxes).each(function () {
+                if (!$(this).hasClass(type)) {
+                    $(this).fadeOut('slow');
+                } else {
+                    $(this).fadeIn();
+                }
+            });
+        }
+    }
+});
+
+
+
